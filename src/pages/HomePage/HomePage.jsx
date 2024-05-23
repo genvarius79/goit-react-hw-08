367e4e043d677d5e5559014558e8e1b9f77999bb
Hennadii Polupan <146132514+genvarius79@users.noreply.github.com>
2024-05-23 01:32:37 +0100

    styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,14 @@
 import PageTitle from "../../components/PageTitle/PageTitle";
+import css from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
-    <div>
-      <PageTitle>Phonebook, welcome to the page</PageTitle>
-      <p>
-        Welcome to the telephone directory. To begin, your account must be
-        registered. After this, you can log into your account to add, delete,
-        and search for a contact by name and phone number. Thank you for using
-        our service!
+    <div className={css.container}>
+      <PageTitle>Welcome to Phonebook! </PageTitle>
+      <p className={css.text}>
+        This application allows you to easily add, delete, and search your
+        contacts by name, or phone number. To use the application, you need to
+        register and log in to your account
       </p>
     </div>
   );
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,10 +1,13 @@
 import PageTitle from "../../components/PageTitle/PageTitle";
 import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+import css from "./RegistrationPage.module.css";
 
 export default function RegisterPage() {
   return (
-    <div>
-      <PageTitle>Register your account</PageTitle>
+    <div className={css.container}>
+      <div className={css.wrapper}>
+        <PageTitle>Register your account</PageTitle>
+      </div>
       <RegistrationForm />
     </div>
   );
